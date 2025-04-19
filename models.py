4c818ed4f6146c4c52aa8358a4e6b9ea5d659104
unknown <wesleyfreire1707@gmail.com>
2025-04-18 23:20:34 -0300

    atualização

--- models.py
+++ models.py
@@ -1,1 +1,7 @@
 from db import db
+
+class contato(db.model):
+    __tablename__ = 'contatos'
+
+    nome = db.Column(db.String(50), nullable=False)
+    telefone = db.Column(db.Integer, nullble=False)
