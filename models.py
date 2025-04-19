221849613dadc94d993f78e61ff8af1fd96158df
unknown <wesleyfreire1707@gmail.com>
2025-04-18 23:58:45 -0300

    correção

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from db import db
 
 
-class contato(db.model):
+class contato(db.Model):
     __tablename__ = 'contatos'
-    id = db.Column(db.Iteger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
-    telefone = db.Column(db.Integer, nullble=False)
+    telefone = db.Column(db.Integer, nullable=False)
 
     def __repr_(self):
         return f"<{self.nome}"
