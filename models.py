682d48e9d3e6980d584d5631fa4e2c59e14cf3b5
unknown <wesleyfreire1707@gmail.com>
2025-04-18 23:56:06 -0300

    atualização

--- models.py
+++ models.py
@@ -1,7 +1,11 @@
 from db import db
 
+
 class contato(db.model):
     __tablename__ = 'contatos'
-
+    id = db.Column(db.Iteger, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
     telefone = db.Column(db.Integer, nullble=False)
+
+    def __repr_(self):
+        return f"<{self.nome}"
