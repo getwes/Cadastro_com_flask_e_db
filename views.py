cf74ba38cbd0cc84b2817521bae7babae5df0b69
unknown <wesleyfreire1707@gmail.com>
2025-04-22 13:26:32 -0300

    atualização

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 from main import app
-from flask import render_template
+from flask import render_template, request
+from models import contato
+from db import db
 #rotas
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/registrar")
+@app.route("/registrar", methods=['GET', 'POST'])
 def registrar():
-    return render_template("registrar_contato.html")
+    if request.method == 'GET'
+        return render_template("registrar_contato.html")
