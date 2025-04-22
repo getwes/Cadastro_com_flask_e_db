ac245be8e20d1ed83a03e49308bd3a3467979323
unknown <wesleyfreire1707@gmail.com>
2025-04-22 14:04:39 -0300

    atualização

--- views.py
+++ views.py
@@ -1,7 +1,5 @@
 from main import app
 from flask import render_template, request
-from models import contato
-from db import db
 #rotas
 
 @app.route("/")
@@ -10,5 +8,13 @@ def homepage():
 
 @app.route("/registrar", methods=['GET', 'POST'])
 def registrar():
-    if request.method == 'GET'
+    if request.method == 'GET':
         return render_template("registrar_contato.html")
+    elif request.method == "POST":
+        nome = request.form['nomeForm']
+        telefone = request.form['telefoneForm']
+
+        print(nome)
+        print(telefone)
+
+        return 'Usuario registrado'
